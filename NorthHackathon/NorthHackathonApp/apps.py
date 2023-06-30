2d91b6789e1e8caf6f353a79b1493db387808ec9
bagkir <bagrecovkirill@yandex.ru>
2023-06-30 15:09:27 +0300

    first commit

---
+++ NorthHackathon/NorthHackathonApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NorthhackathonappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NorthHackathonApp'
---
+++ NorthHackathon/NorthHackathonApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import ArhangelskayaOblast
+from rest_framework.response import Response
+
+
+# Create your views here.
+
