87f5e95b1a0733812ce3fe1ba5d1c91d33cbbcf3
Shirkunov Artyom <sshiae@localhost.localdomain>
2023-07-02 14:17:24 +0300

    Добавил Docker и docker-compose

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from apps.api.exceptions import InvalidRequestException, UserNameAlreadyExistException
+from apps.api.exceptions import InvalidRequestException
 from .services.object_tourism_service import *
 from .services.username_service import *
 
