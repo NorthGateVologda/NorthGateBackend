24aebad66a301f5cecc5d7a692db31dec988a02b
Ширкунов Артём Евгеньевич <shiae@maxi-net.ru>
2023-07-28 17:36:32 +0300

    Восстанавливаем авторизацию по токенам

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,12 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
